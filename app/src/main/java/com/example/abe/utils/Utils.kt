b93560ab752fba6746bc97df7992f5049d79f376
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-04-05 05:03:04 +0700

    feat: add utils for input output validation

--- app/src/main/java/com/example/abe/utils/Utils.kt
+++ app/src/main/java/com/example/abe/utils/Utils.kt
@@ -1,7 +1,17 @@
 package com.example.abe.utils
 
 import com.example.abe.connection.ConnectivityObserver
+import org.jsoup.Jsoup
+import org.jsoup.safety.Safelist
 
 fun isConnected(networkState: ConnectivityObserver.NetworkState?): Boolean {
     return (networkState != null && (networkState == ConnectivityObserver.NetworkState.AVAILABLE || networkState == ConnectivityObserver.NetworkState.LOSING))
+}
+
+fun isNumeric(input: String): Boolean {
+    return input.matches("\\d+".toRegex())
+}
+
+fun sanitizeHtml(input: String): String {
+    return Jsoup.clean(input, Safelist.basic())
 }
